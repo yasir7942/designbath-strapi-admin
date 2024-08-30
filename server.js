695c30bc29b85171c82ae368722d6426e95526da
yasir aslam <yasir7942@outlook.com>
2024-08-30 14:15:14 +0500

    first upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const strapi = require('@strapi/strapi');
+strapi().start();
+
+/*
+const strapi = require('@strapi/strapi');
+
+strapi({
+    autoReload: true,
+    serveAdminPanel: true
+}).start();
+
+*/
---
+++ types/generated/components.d.ts
@@ -0,0 +1,21 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface LayoutVideoLinks extends Schema.Component {
+  collectionName: 'components_video_links_video_links';
+  info: {
+    displayName: 'VideoLink';
+    icon: 'link';
+    description: '';
+  };
+  attributes: {
+    url: Attribute.String;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'layout.video-links': LayoutVideoLinks;
+    }
+  }
+}
